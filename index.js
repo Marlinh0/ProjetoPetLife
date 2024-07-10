e9e36deb2d011f354a571e0d09f4c3128e98dd39
F0gacinhA <134811053+F0gacinhA@users.noreply.github.com>
2024-07-10 20:16:02 -0300

    Add scrollreveal

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ elementosDuvida.forEach(function(duvida){
     })
 })
 
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('#inicio',{ duration: 1000});
+
+sr.reveal('#clinica',{ duration: 2000});
+
+sr.reveal('#farmacia',{ duration: 2000});
+
+sr.reveal('#duvidas',{ duration: 2000});
+
